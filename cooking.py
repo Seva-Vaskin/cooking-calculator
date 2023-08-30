f8a615dd8218ec32cd7fe5c81b7ca9abb171bf5c
Vsevolod Vaskin <vaskin.2003@gmail.com>
2023-08-30 23:20:16 +0300

    split index.html into base.html and index.html

--- cooking.py
+++ cooking.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return render_template('index.html')
@@ -9,4 +10,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
